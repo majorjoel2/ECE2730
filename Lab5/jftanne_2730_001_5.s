d66a5f8e37b587444b79b729cde9e194356ed0a6
Joel Tanner <jftanne@g.clemson.edu>
2020-03-23 09:07:23 -0400

    Added header and comments recompiled and verifed it still worked

--- Lab5/jftanne_2730_001_5.s
+++ Lab5/jftanne_2730_001_5.s
@@ -1,5 +1,12 @@
-/* insert program header here*/
-
+/*
+NAME    Joel Tanner
+COURSE  ECE 2730
+SECTION  001
+DATE    03/23/2020
+FILE    jftanne_2730_001_5.s
+PURPOSE  This file will calculate the Fibonacci Sequence with a recursive
+				 assembly function.
+*/
 /* begin assembly stub */
 
 /* insert Fib function header here*/
@@ -13,30 +20,40 @@ Fib:
 	subl $8, %esp
 
 	/* put code here */
+	#local_var = global_var;
 	movl global_var, %ebx
 	movl %ebx, -4(%ebp)
+	#if(local_var == 0) return;
 	cmpl $0, -4(%ebp)
 	jne L00
 	jmp return
 
 L00:
+	#if(local_var == 1) return;
 	cmpl $1, -4(%ebp)
 	jne L01
 	jmp return
 
 L01:
+	#global_var = local_var - 1;
 	movl -4(%ebp), %ebx
 	subl $1, %ebx
 	movl %ebx, global_var
+	#Fib();
 	call Fib
+	#temp_var = global_var;
 	movl global_var, %ebx
 	movl %ebx, -8(%ebp)
+	#global_var = local_var - 2;
 	movl -4(%ebp), %ebx
 	subl $2, %ebx
 	movl %ebx, global_var
+	#Fib();
 	call Fib
+	#temp_var = temp_var + global_var;
 	movl global_var, %ebx
 	addl %ebx, -8(%ebp)
+	#global_var = temp_var;
 	movl -8(%ebp), %ebx
 	movl %ebx, global_var
 
