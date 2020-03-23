f92a3f6a3526b5c425fac84a691da02619c3f8e8
Joel Tanner <jftanne@g.clemson.edu>
2020-03-23 09:01:25 -0400

    Code working

--- Lab5/jftanne_2730_001_5.s
+++ Lab5/jftanne_2730_001_5.s
@@ -10,10 +10,11 @@ Fib:
 	pushl %ebx
 	pushl %ebp
 	movl %esp, %ebp
-	subl $8, $esp
+	subl $8, %esp
 
 	/* put code here */
-	movl global_var, -4(%ebp)
+	movl global_var, %ebx
+	movl %ebx, -4(%ebp)
 	cmpl $0, -4(%ebp)
 	jne L00
 	jmp return
@@ -24,14 +25,28 @@ L00:
 	jmp return
 
 L01:
-	movl -4(%ebp), %eax
-	subl 1, %eax
-	movl %eax, global_var
-
+	movl -4(%ebp), %ebx
+	subl $1, %ebx
+	movl %ebx, global_var
+	call Fib
+	movl global_var, %ebx
+	movl %ebx, -8(%ebp)
+	movl -4(%ebp), %ebx
+	subl $2, %ebx
+	movl %ebx, global_var
+	call Fib
+	movl global_var, %ebx
+	addl %ebx, -8(%ebp)
+	movl -8(%ebp), %ebx
+	movl %ebx, global_var
 
 
 	return:
 	/* epilog */
+	addl $8, %esp
+	movl %ebp, %esp
+	popl %ebp
+	popl %ebx
 
 	ret
 /* end assembly stub */
