752acf0b54aa1b4f7b71f0800d6a8dc802f6c883
Joel Tanner <jftanne@g.clemson.edu>
2020-03-30 13:20:56 -0400

    Compiles but has logic issues

--- Lab6/temp-name_2730_001_6.s
+++ Lab6/temp-name_2730_001_6.s
@@ -5,16 +5,45 @@
 /* put function header here */
 
 /* put assembler directives here */
-
+.globl Factorial
+.type Factorial,@function
 Factorial:
 	/* prolog */
+	pushl %ebx
+	pushl %ebp
+	movl %esp, %ebp
+	subl $4, %esp
 
 	/* put code here */
+	cmpl $0, -4(%ebp)
+	jne L01
+	movl $1, %eax
+	jmp return
+
+L01:
+	cmpl $1, -4(%ebp)
+	jne L02
+	movl $1, %eax
+	jmp return
+
+L02:
+	#return n * Factorial(n-1);
+	movl -4(%ebp), %ebx
+	subl $1, %ebx
+	#pass in n-1
+	pushl %ebx
+	call Factorial
+	addl $4, %esp
+	#function output in %eax
+	movl -4(%ebp), %ebx
+	mull %ebx
 
 	return:
 
 	/* epilog */
-
+	movl %ebp, %esp
+	popl %ebp
+	popl %ebx
 	ret
 
 /* end assembly stub */
