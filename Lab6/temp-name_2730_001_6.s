10196eef1896748e5d1c8a30a2ba8dad94f80347
Joel Tanner <jftanne@g.clemson.edu>
2020-03-30 12:42:22 -0400

    Start of Lab

---
+++ Lab6/temp-name_2730_001_6.s
@@ -0,0 +1,20 @@
+/* put program header here */
+
+/* begin assembly stub */
+
+/* put function header here */
+
+/* put assembler directives here */
+
+Factorial:
+	/* prolog */
+
+	/* put code here */
+
+	return:
+
+	/* epilog */
+
+	ret
+
+/* end assembly stub */
